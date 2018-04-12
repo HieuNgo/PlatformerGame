7018eb32407ec279f531b19dbd89f65c18280210
Hieu Ngo (Henry) <ngohieu241@gmail.com>
2018-04-12 14:29:58 -0400

    Upload the Mario game

---
+++ src/com/brackeen/javagamebook/tilegame/sprites/SuperFly.java
@@ -0,0 +1,59 @@
+package com.brackeen.javagamebook.tilegame.sprites;
+
+import com.brackeen.javagamebook.codereflection.CodeReflection;
+import com.brackeen.javagamebook.graphics.Animation;
+
+public class SuperFly extends Fly{
+	/**
+    A super Fly is a giant fly .
+*/
+
+//    private Throwable e = new Throwable();
+    
+    public SuperFly(Animation left, Animation right,
+        Animation deadLeft, Animation deadRight)
+    {
+        super(left, right, deadLeft, deadRight);
+    	if(CodeReflection.isTracing() && SpritesPackageTracingEnabled.getSpritesPackageTracingEnabledInstance().isEnabled()) {
+        	if(CodeReflection.getAbstactionLevel()>=1)
+        	{//check to make sure it's this level of abstraction
+        		e.fillInStackTrace();		
+        		CodeReflection.registerMethod(e.getStackTrace()[0].getClassName(),
+        								e.getStackTrace()[0].getMethodName());
+        	}
+    	}
+    	//We don't apply gravity to the fly
+        flying = true;
+
+    }
+
+
+    public float getMaxSpeed() {
+    	if(CodeReflection.isTracing() && SpritesPackageTracingEnabled.getSpritesPackageTracingEnabledInstance().isEnabled()) {
+        	if(CodeReflection.getAbstactionLevel()>=2)
+        	{//check to make sure it's this level of abstraction
+        		e.fillInStackTrace();		
+        		CodeReflection.registerMethod(e.getStackTrace()[0].getClassName(),
+        								e.getStackTrace()[0].getMethodName());
+        	}
+    	}
+        return 0.4f * enemySpeedMultiplier;
+    }
+
+
+    public boolean isFlying() {
+    	
+    	if(CodeReflection.isTracing() && SpritesPackageTracingEnabled.getSpritesPackageTracingEnabledInstance().isEnabled()) {
+        	if(CodeReflection.getAbstactionLevel()>=4)
+        	{//check to make sure it's this level of abstraction
+        		e.fillInStackTrace();		
+        		CodeReflection.registerMethod(e.getStackTrace()[0].getClassName(),
+        								e.getStackTrace()[0].getMethodName());
+        	}
+    	}
+        return isAlive() && super.isFlying();
+    }
+
+}
+
+
